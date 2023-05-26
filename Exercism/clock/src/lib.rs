2aa51153ecc72e261cc42ee967be560b1b5b18ef
Marco Fanelli <marcovittorio789@gmail.com>
2023-05-26 17:03:53 +0200

    improved-clock

--- Exercism/clock/src/lib.rs
+++ Exercism/clock/src/lib.rs
@@ -5,15 +5,15 @@ pub struct Clock {
     hours: i32,
     minutes: i32,
 }
-const HOURS_PER_DAY: i32 = 24;
+
 const MINUTES_PER_HOUR: i32 = 60;
-const MINUTES_PER_DAY: i32 = MINUTES_PER_HOUR * HOURS_PER_DAY;
+const MINUTES_PER_DAY: i32 = 24 * MINUTES_PER_HOUR;
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let total_minutes = total_minutes(hours, minutes);
-        let hours = total_minutes / MINUTES_PER_HOUR;
-        let minutes = total_minutes % MINUTES_PER_HOUR;
+        let total_minutes = (hours * MINUTES_PER_HOUR + minutes).rem_euclid(MINUTES_PER_DAY);
+        let hours = total_minutes.div_euclid(MINUTES_PER_HOUR);
+        let minutes = total_minutes.rem_euclid(MINUTES_PER_HOUR);
         Self { hours, minutes }
     }
 
@@ -25,14 +25,5 @@ impl Clock {
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:02}:{:02}", self.hours, self.minutes)
-    }
-}
-
-fn total_minutes(hours: i32, minutes: i32) -> i32 {
-    let total_minutes = (hours * MINUTES_PER_HOUR + minutes) % MINUTES_PER_DAY;
-    if total_minutes >= 0 {
-        total_minutes
-    } else {
-        total_minutes + MINUTES_PER_DAY
     }
 }
