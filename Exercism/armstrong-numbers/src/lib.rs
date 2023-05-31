21f47a6c4d8795d606e61d089f761127e4ae83fc
Marco Fanelli <marcovittorio789@gmail.com>
2023-05-31 10:33:21 +0200

    prime-factors.rs

--- Exercism/armstrong-numbers/src/lib.rs
+++ Exercism/armstrong-numbers/src/lib.rs
@@ -1,8 +1,9 @@
 pub fn is_armstrong_number(num: u32) -> bool {
     let num_string = num.to_string();
-    num_string
+    let sum: u64 = num_string
         .chars()
-        .map(|c| c.to_digit(10).unwrap().pow(num_string.len() as u32))
-        .sum::<u32>()
-        == num
+        .map(|c| c.to_digit(10).unwrap().pow(num_string.len() as u32) as u64)
+        .sum();
+
+    sum == num as u64
 }
